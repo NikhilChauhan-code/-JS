3fbad3ab355f031e524725dbd04ffb1e13803b33
NikhilChauhan-code <nikhilchauhan94596@gmail.com>
2024-12-03 18:39:56 +0530

    operations

--- 01_basics/03_conversionOprations.js
+++ 01_basics/03_conversionOprations.js
@@ -18,7 +18,9 @@ let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+
 // let valueInBoolean = 10
 
 // let booleanvalueInBoolean = Boolean(valueInBoolean)
@@ -29,3 +31,48 @@ console.log(booleanIsLoggedIn);
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+let value =3
+let negvalue = -value
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + "2");
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let GameCounter = 100
+GameCounter++ //postfix
+++GameCounter //prefix here they are same but they are not same all the time
+console.log(GameCounter);
