09b999c6b337a1c46eb91805041a43d8a41ea397
NikhilChauhan-code <nikhilchauhan94596@gmail.com>
2024-12-04 10:21:08 +0530

    strings

---
+++ 01_basics/05_String.js
@@ -0,0 +1,53 @@
+const name = "Nikhil "
+const repocount =50
+// console.log(name + repocount + " value")
+
+console.log(`hello my name is ${name}and my repo count is ${repocount}`);
+
+const gameName = new String('nikhil-nc')
+
+
+// console.log(gameName)
+// console.log(gameName[0])
+// console.log(gameName[1])
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase())
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf("k"))
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-1, 8)
+// const anotherString1 = gameName.slice(-2, 7)
+// const anotherString2= gameName.slice(-3, 6)
+// const anotherString3 = gameName.slice(-4, 5)
+// const anotherString4 = gameName.slice(-5, 4)
+// const anotherString5 = gameName.slice(-6, 3)
+// const anotherString6 = gameName.slice(-7, 2)
+// const anotherString7 = gameName.slice(-8, 1)
+// const anotherString8 = gameName.slice(-0, 3)
+
+// console.log(anotherString);
+// console.log(anotherString1);
+// console.log(anotherString2);
+// console.log(anotherString3);
+// console.log(anotherString4);
+// console.log(anotherString5);
+// console.log(anotherString6);
+// console.log(anotherString7);
+// console.log(anotherString8);
+
+const newString = "    nikhil    "
+console.log(newString)
+console.log(newString.trim())
+
+const url ="https://hites.com/hitesh%20choudary"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('hitesh'))
+console.log(url.includes('nikhil'))
+
+console.log(gameName.split('-'))
+
