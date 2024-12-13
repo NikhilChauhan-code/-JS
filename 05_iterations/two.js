f6b43b3e0a46b308023c160001832680453dea94
NikhilChauhan-code <nikhilchauhan94596@gmail.com>
2024-12-13 19:47:09 +0530

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,44 @@
+
+
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index++
+// }
+
+
+
+let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++
+// }
+
+let score = 6
+
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+// this is the special case in do while in this condition is not true beacuse 12 is greater then 10 but it still print 12 beacuse conditon comes in the end in other loop like for or while code would not print anything
+let score1 = 12
+
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score1 <= 10);  
+
